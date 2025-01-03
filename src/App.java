71de5d65ba35bd3c182e2f85328c8d238ccd7630
Pedrolag <luisaguilargarciapedro@gmail.com>
2025-01-02 19:41:11 -0800

    Added new t

--- src/App.java
+++ src/App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!dsfsdjsf");
-        Integer i=5;
+        Integer i=5,t;
+
     }
 }
